90a0549d3139c8493c08e10205c452f469904bfd
Andrew Kozlov <adkozlov@gmail.com>
2013-03-27 20:46:33 +0400

    Visualizer improved 2.

--- src/visualizer/Visualizer.java
+++ src/visualizer/Visualizer.java
@@ -5,6 +5,7 @@ import edu.uci.ics.jung.graph.DelegateForest;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedOrderedSparseMultigraph;
 import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
+import edu.uci.ics.jung.visualization.RenderContext;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.decorators.EdgeShape;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
@@ -35,7 +36,7 @@ public class Visualizer implements Runnable {
     public void run() {
         try {
             Tree root = new Parser().parse(new FileInputStream(FILENAME));
-            visualize(root);
+            makeVertexFromTree(new Vertex(root.getNode()), root);
 
             JFrame frame = new JFrame();
             frame.getContentPane().add(new GraphZoomScrollPane(makeVisualizationViewer(new TreeLayout<Vertex, Edge>(new DelegateForest<Vertex, Edge>(graph)))));
@@ -49,52 +50,41 @@ public class Visualizer implements Runnable {
         }
     }
 
-    private void convert(Vertex root, Tree tree) {
+    private void makeVertexFromTree(Vertex root, Tree tree) {
         for (Tree child : tree.getChildren()) {
             Vertex childVertex = new Vertex(child.getNode());
             graph.addEdge(new Edge(), root, childVertex);
-            convert(childVertex, child);
+            makeVertexFromTree(childVertex, child);
         }
     }
 
     private VisualizationViewer<Vertex, Edge> makeVisualizationViewer(TreeLayout<Vertex, Edge> treeLayout) {
         VisualizationViewer result = new VisualizationViewer<Vertex, Edge>(
                 treeLayout, new Dimension(1024, 768));
+        RenderContext context = result.getRenderContext();
 
-        // sets edges style
-        result.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line());
-        // sets how to show cast Label to String
-        result.getRenderContext().setVertexLabelTransformer(new Transformer<Vertex, String>() {
+        // edges style
+        context.setEdgeShapeTransformer(new EdgeShape.QuadCurve<Vertex, Edge>());
+        context.setEdgeStrokeTransformer(new ConstantTransformer<Stroke>(new BasicStroke(2f)));
+        context.setArrowFillPaintTransformer(new ConstantTransformer<Paint>(Color.BLACK));
+
+        // vertices style
+        Ellipse2D ellipse = new Ellipse2D.Double(-25, -15, 50, 30);
+        context.setVertexShapeTransformer(new ConstantTransformer<Shape>(ellipse));
+        context.setVertexLabelTransformer(new Transformer<Vertex, String>() {
             public String transform(Vertex vertex) {
                 return vertex.toString();
             }
         });
-        // sets edge thickness
-        result.getRenderContext().setEdgeStrokeTransformer(new ConstantTransformer<Stroke>(new BasicStroke(3f)));
-        // sets position of Label
-        result.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
-        // sets vertex color
-        result.getRenderContext().setVertexFillPaintTransformer(new Transformer<Vertex, Paint>() {
+        context.setVertexFillPaintTransformer(new Transformer<Vertex, Paint>() {
             public Paint transform(Vertex vertex) {
                 return vertex.isTerminal() ? Color.WHITE : Color.LIGHT_GRAY;
             }
         });
-
-        // sets vertex size
-        Ellipse2D circle = new Ellipse2D.Double(-15, -15, 30, 30);
-        result.getRenderContext().setVertexShapeTransformer(new ConstantTransformer<Shape>(circle));
-
+        result.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
         result.setVertexToolTipTransformer(new ToStringLabeller());
-        result.getRenderContext().setArrowFillPaintTransformer(new ConstantTransformer<Paint>(Color.WHITE));
 
         return result;
-    }
-
-    public void visualize(Tree tree) {
-        Vertex root = new Vertex(tree.getNode());
-        convert(root, tree);
-
-
     }
 }
 
